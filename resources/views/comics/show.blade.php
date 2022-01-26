75dd31efddcd245e290bb64dc861cc39036e48d2
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-26 18:09:02 +0100

    add span

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -8,7 +8,8 @@
                 <img src="{{$comic->src}}" alt="{{$comic->series}}" class="w-100">
             </div>
             <div class="col-8">
-                <h1>{{$comic->series}}</h1>
+                <h1>{{$comic->title}}</h1>
+                <span><strong>Series</strong>: {{$comic->series}}</span>
                 <p class="fs-4 my-5">{{$comic->description}}</p>
                 <span class="fs-5"><strong>Price</strong>: {{$comic->price}} €</span>
             </div>
